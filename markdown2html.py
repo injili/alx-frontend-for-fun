b236bf9663c9a18ef0e4aa2cc79e80b00c5a17d7
injili <nyarekigospel@gmail.com>
2024-03-24 12:25:31 +0000

    start a script

--- markdown2html.py
+++ markdown2html.py
@@ -12,8 +12,8 @@ def main():
     """
     Converts Markdown to HTML
     """
-    if len(sys.args) < 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+    if len(sys.argv) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         sys.exit(1)
     else:
         sys.exit(0)
